971d78f12ddc6237b23802c0ee62426dc2918a63
Billy19191 <bill.ratchanon@gmail.com>
2024-05-01 02:35:42 +0700

    chore: use axios instead of fetch

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 export default function Home() {
   const [data, setData] = useState<any>('loading.:')
 
   const fetchData = async () => {
-    const response = await fetch('http://localhost:3001/api/home')
-    const data = await response.json()
-    console.log(data.message)
-    setData(data.message)
+    // const response = await fetch('http://localhost:3001/api/home')
+    // const data = await response.json()
+    // console.log(data.message)
+    // setData(data.message)
+    const res = await axios.get('http://localhost:3001/api/home')
+    console.log(res.data.message)
+    setData(res.data.message)
   }
   fetchData()
   return (
