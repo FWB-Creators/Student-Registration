0e7b1ae4b15b50ac42578e2a1d40009a7f5fe4c2
Sikares <sikares.n@gmail.com>
2024-05-02 03:04:47 +0700

    Fix

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -4,14 +4,16 @@ import Image from 'next/image'
 
 function App() {
     return (
-    <div className="flex w-full h-screen bg-gray-50">
-        <div className="hidden lg:flex h-full w-8/12 items-center justify-center bg-[#FFA842]">
-            <Image src="/static/Fox.png" alt="Fox" width={550} height={550} />
+        <div className="flex w-full h-screen bg-gray-50">
+            <div className="hidden lg:flex h-full w-8/12 items-center justify-center bg-[#FFA842]">
+                <div className="active:scale-[.98] active:duration-75 transition-all hover:scale-[1.05] ease-in-out">
+                    <Image src="/static/Fox.png" alt="Fox" width={550} height={550} />
+                </div>
+            </div>
+            <div className="w-full flex items-center justify-center">
+                <Form />
+            </div>
         </div>
-        <div className="w-full flex items-center justify-center">
-            <Form />
-        </div>
-    </div>
     )
 }
 
