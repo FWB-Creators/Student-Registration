d20090d96c38e309a4eab453915307270cd75662
Billy19191 <bill.ratchanon@gmail.com>
2024-05-01 16:34:55 +0700

    feat: mysql integrate

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,33 @@
 import express from 'express'
 import cors from 'cors'
+import mysql from 'mysql2'
 
 const app = express()
 const port = 3001
 
 // Use cors middleware, this will allow all CORS requests
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mydb',
+})
+
 app.use(
   cors({
     origin: 'http://localhost:3000',
   })
 )
 
-app.get('/api/home', (req, res) => {
-  res.json({ message: 'Hello from the server!' })
+app.get('/api/home', (req, res, next) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.log('An error occurred:', err)
+      return res.json({ message: 'An error occurred' })
+    }
+    console.log('Query result:', results)
+    return res.json({ users: results })
+  })
 })
 
 app.listen(port, () => {
