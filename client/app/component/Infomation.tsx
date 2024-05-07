0e534d88b2837fd067dfda1fe0331070e6582ded
Sikares <sikares.n@gmail.com>
2024-05-07 20:59:00 +0700

    bie

--- client/app/component/Infomation.tsx
+++ client/app/component/Infomation.tsx
@@ -69,6 +69,12 @@ export default function Information() {
                         <input className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your password' type='password'/>
                     </div>
                 </div>
+                <div className="flex justify-between">
+                    <div className='flex-1 mx-5'>
+                        <label className='text-lg font-medium'>Address</label>
+                        <input className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent' placeholder='Enter your address' />
+                    </div>
+                </div>
                 <button className='mt-8 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out py-3 rounded-xl bg-orange-500 text-white text-lg font-bold'>Confirm</button>
             </div>
         </div>
